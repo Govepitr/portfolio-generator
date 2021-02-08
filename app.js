1a1bf6abd0395b474ea507479539bc557906c5bc
Dan Arbelo <govepitr@gmail.com>
2021-02-08 08:08:46 -0700

    added when method for conditional prompts

--- app.js
+++ app.js
@@ -29,14 +29,20 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
       message: 'Provide some information about yourself:',
-      validate: bioInput => {
-        if (bioInput) {
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
           return true;
-        }else{
-          console.log("I'd like to get to know those are about to be in my thrall.");
+        } else {
+          return false;
         }
       }
     }
